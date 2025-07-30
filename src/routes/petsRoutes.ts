160171808b5f3c0df1df3f552e5c893e471fbba4
Hugo Moreira <hugo.cesar91@gmail.com>
2025-07-30 18:07:55 -0300

    Adicionados filtros de busca para a rota de listagem dos pets.

--- src/routes/petsRoutes.ts
+++ src/routes/petsRoutes.ts
@@ -1,16 +1,36 @@
 import { Router } from "express";
-import { PrismaClient } from "../generated/prisma";
+import { pet_size, pet_type, PrismaClient } from "../generated/prisma";
 
 const petsRoutes = Router();
 const prisma = new PrismaClient();
 
-petsRoutes.get("/", async (_, res) => {
-  try {
-    const pets = await prisma.pet.findMany();
+const validTypes: pet_type[] = ["dog", "cat", "other"];
+const validSizes: pet_size[] = ["small", "medium", "large"];
 
-    if (!pets) {
-      return res.status(404).send({ message: "List of pets not found" });
+petsRoutes.get("/", async (req, res) => {
+  const { type, size, available } = req.query;
+  const filters: { type?: pet_type; size?: pet_size; available?: boolean } = {};
+
+  if (type && validTypes.includes(type.toString() as pet_type)) {
+    filters.type = type.toString() as pet_type;
+  }
+
+  if (size && validSizes.includes(size.toString() as pet_size)) {
+    filters.size = size.toString() as pet_size;
+  }
+
+  if (typeof available === "string") {
+    if (available === "true") {
+      filters.available = true;
+    } else if (available === "false") {
+      filters.available = false;
     }
+  }
+
+  try {
+    const pets = await prisma.pet.findMany({
+      where: filters,
+    });
 
     res.json(pets);
     // eslint-disable-next-line @typescript-eslint/no-unused-vars
