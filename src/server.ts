c6d87e45838ab6fa62de1ca1f94aa9afa60b51f6
Hugo Moreira <hugo.cesar91@gmail.com>
2025-08-04 15:47:22 -0300

    Swagger da rota de listagem dos pets.

--- src/server.ts
+++ src/server.ts
@@ -2,11 +2,14 @@ import express from "express";
 import petsRoutes from "./routes/petsRoutes";
 import adoptersRoutes from "./routes/adoptersRoutes";
 import adoptionsRoutes from "./routes/adoptionsRoutes";
+import swaggerUi from "swagger-ui-express";
+import swaggerDocument from "../swagger.json";
 
 const port = 3000;
 const app = express();
 
 app.use(express.json());
+app.use("/docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 app.use("/pets", petsRoutes);
 app.use("/adopters", adoptersRoutes);
